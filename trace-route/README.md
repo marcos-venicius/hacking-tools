41c847a41ce7f3cf7e4f5c65be26341f8852452f
Marcos Venicius <marcosdevelopment.business@gmail.com>
2024-02-03 15:02:09 -0500

    feat: add trace-route script

---
+++ trace-route/README.md
@@ -0,0 +1,29 @@
+# Trace Route
+
+Basically the concept works using the TTL parameter of the IP protocol.
+
+When you send a packet using the IP protocol, automatically it receives a default TTL (Time To Live) value,
+this TTL may be 64, 128, 255, or any other customized by a user.
+
+**What is the purpose of the TTL?**
+
+When a packet is created and sent with a TTL 10, for example, the router receives this packet and decrements your value by 1
+meaning that the next router that get this packet will reach the TTL value being 9 and this process
+happens every time that this packets pass by a new router, this process is called HOP.
+
+So, when the TTL reachs 0 the router returns a ICMP response with the type 11 and code 0, meaning that
+this packet has expired, so we can get the router ip.
+
+By incrementing the TTL value one by one (starting at 1) we can keep track the routers in the packet transmition path.
+
+## Running
+
+* This script should run as sudo
+* This script needs a parameter that is the target you want to trace the route
+    * it cannot be a domain, to transform the domain in a IP you can use my [DNS Resolver tool](../dns-resolver/) that is free.
+
+Example:
+
+```bash
+sudo ./trace-route.py 10.0.0.4
+```
---
+++ trace-route/trace-route.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+
+from scapy.all import IP, ICMP, sr1
+
+def ip_is_valid(ip):
+    import re
+
+    pattern = re.compile("\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}")
+    return pattern.match(ip)
+
+def author():
+    print("""
+████████╗██████╗  █████╗  ██████╗███████╗    ██████╗  ██████╗ ██╗   ██╗████████╗███████╗
+╚══██╔══╝██╔══██╗██╔══██╗██╔════╝██╔════╝    ██╔══██╗██╔═══██╗██║   ██║╚══██╔══╝██╔════╝
+   ██║   ██████╔╝███████║██║     █████╗      ██████╔╝██║   ██║██║   ██║   ██║   █████╗  
+   ██║   ██╔══██╗██╔══██║██║     ██╔══╝      ██╔══██╗██║   ██║██║   ██║   ██║   ██╔══╝  
+   ██║   ██║  ██║██║  ██║╚██████╗███████╗    ██║  ██║╚██████╔╝╚██████╔╝   ██║   ███████╗
+   ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚══════╝    ╚═╝  ╚═╝ ╚═════╝  ╚═════╝    ╚═╝   ╚══════╝
+                                                                                        
+by @marcos-venicius, https://github.com/marcos-venicius
+""")
+
+def params():
+    import sys
+
+    if len(sys.argv) != 2:
+        print('Usage: sudo ./trace-route.py <ip>')
+        exit(1)
+
+    if not ip_is_valid(sys.argv[1]):
+        print('\033[1;31m[!] Invalid IP address\033[0m')
+        exit(1)
+
+    return sys.argv[1]
+
+def is_sudo():
+    import os
+
+    return os.geteuid() == 0
+
+def trace(ttl, ip):
+    ip = IP(
+        dst=ip,
+        ttl=ttl
+    )
+
+    icmp = ICMP()
+
+    return sr1(ip / icmp, verbose=False, timeout=1)
+
+if not is_sudo():
+    print('\033[1;31m[!] You should run as sudo\033[0m')
+    exit(1)
+
+ip = params()
+
+author()
+
+print('\033[0;36m[*] Press ctrl+c when you want to stop\033[0m\n')
+print('\033[1;37mTracing route...\033[0m\n')
+
+try:
+    found, ttl, counter = set(), 1, 0
+
+    while True:
+        res = trace(counter, ip)
+
+        if res:
+            if res.src in found:
+                break
+            else:
+                found.add(res.src)
+
+            counter += 1
+
+            print(f'[{str(counter).ljust(3, " ")}] {res.src}')
+
+        counter += 1
+
+    print('\n\033[1;32m[+] Tracing finished\033[0m\n')
+except:
+    exit(0)
