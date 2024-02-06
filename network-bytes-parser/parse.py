2a63c70024d76a666c385bdc0f3f4c46a4ea5665
Marcos Venicius <marcosdevelopment.business@gmail.com>
2024-02-06 13:37:16 -0300

    improve parser

--- network-bytes-parser/parse.py
+++ network-bytes-parser/parse.py
@@ -22,76 +22,105 @@ def read_bytes(file):
 
         return result
 
-class Parser:
+class Ethernet:
     def __init__(self, byts):
-        self.byts = byts
+        self.byts = byts[0:14]
 
     def __parse_destination_mac(self):
         return ':'.join([byt.upper() for byt in self.byts[0:6]])
 
-    def __parse_origin_mac(self):
+    def __parse_source_mac(self):
         return ':'.join([byt.upper() for byt in self.byts[6:12]])
 
-    def __parse_protocol_type(self):
+    def __parse_protocol(self):
         pt = {'0800': 'IPv4', '0806': 'ARP'}
 
-        return pt[''.join(self.byts[12:14])]
-
-    def __parse_arp_hardware_type(self):
-        ht = {'0001': 'Ethernet'}
-
-        return ht[''.join(self.byts[14:16])]
-
-    def __parse_arp_protocol_type(self):
-        pt = {'0800': 'IPv4'}
-
-        return pt[''.join(self.byts[16:18])]
-    
-    def __parse_arp_hardware_size(self):
-        return int('0x' + self.byts[18], 16)
-
-    def __parse_arp_protocol_size(self):
-        return int('0x' + self.byts[19], 16)
-
-    def __parse_arp_opcode(self):
-        return int('0x' + ''.join(self.byts[20:22]), 16)
-
-    def __parse_arp_sender_mac(self):
-        return ':'.join([b.upper() for b in self.byts[22:28]])
-
-    def __parse_arp_sender_ip(self):
-        return '.'.join([str(int('0x' + b, 16)) for b in self.byts[28:32]])
-
-    def __parse_arp_target_mac(self):
-        return ':'.join([b.upper() for b in self.byts[32:38]])
-
-    def __parse_arp_target_ip(self):
-        return '.'.join([str(int('0x' + b, 16)) for b in self.byts[38:42]])
-
-    def __parse_arp_padding(self):
-        return ''.join([i.upper() for i in self.byts[42:]])
+        return pt[''.join(self.byts[12:])]
 
     def parse(self):
+        return {
+            'destination_mac': self.__parse_destination_mac(),
+            'source_mac': self.__parse_source_mac(),
+            'protocol': self.__parse_protocol(),
+            'padding': ''
+        }
+
+class ARP:
+    def __init__(self, byts):
+        self.byts = byts[14:]
+
+    def __parse_hardware_type(self):
+        ht = {'0001': 'Ethernet'}
+
+        return ht[''.join(self.byts[:2])]
+
+    def __parse_protocol_type(self):
+        pt = {'0800': 'IPv4'}
+
+        return pt[''.join(self.byts[2:4])]
+    
+    def __parse_hardware_size(self):
+        return int('0x' + self.byts[4], 16)
+
+    def __parse_protocol_size(self):
+        return int('0x' + self.byts[5], 16)
+
+    def __parse_opcode(self):
+        return int('0x' + ''.join(self.byts[6:8]), 16)
+
+    def __parse_sender_mac(self):
+        return ':'.join([b.upper() for b in self.byts[8:14]])
+
+    def __parse_sender_ip(self):
+        return '.'.join([str(int('0x' + b, 16)) for b in self.byts[14:18]])
+
+    def __parse_target_mac(self):
+        return ':'.join([b.upper() for b in self.byts[18:24]])
+
+    def __parse_target_ip(self):
+        return '.'.join([str(int('0x' + b, 16)) for b in self.byts[24:28]])
+
+    def __parse_padding(self):
+        return ''.join([i.upper() for i in self.byts[28:]])
+
+    def parse(self):
+        return {
+            'padding': self.__parse_padding(),
+            'hardware_type': self.__parse_hardware_type(),
+            'protocol_type': self.__parse_protocol_type(),
+            'hardware_size': self.__parse_hardware_size(),
+            'protocol_size': self.__parse_protocol_size(),
+            'opcode': self.__parse_opcode(),
+            'sender_mac_address': self.__parse_sender_mac(),
+            'sender_ip_address': self.__parse_sender_ip(),
+            'target_mac_address': self.__parse_target_mac(),
+            'target_ip_address': self.__parse_target_ip()
+        }
+
+class Parser:
+    def __init__(self, byts):
+        self.byts = byts
+
+    def parse(self):
+        eth = Ethernet(self.byts)
+
         result = {
-            'Ethernet': {
-                'destination_mac': self.__parse_destination_mac(),
-                'origin_mac': self.__parse_origin_mac(),
-                'protocol': self.__parse_protocol_type(),
-            }
+            'Ethernet': eth.parse()
         }
 
         if result['Ethernet']['protocol'] == 'ARP':
+            arp = ARP(byts).parse()
             result['ARP'] = {}
-            result['Ethernet']['padding'] = self.__parse_arp_padding()
-            result['ARP']['hardware_type'] = self.__parse_arp_hardware_type()
-            result['ARP']['protocol_type'] = self.__parse_arp_protocol_type()
-            result['ARP']['hardware_size'] = self.__parse_arp_hardware_size()
-            result['ARP']['protocol_size'] = self.__parse_arp_protocol_size()
-            result['ARP']['opcode'] = self.__parse_arp_opcode()
-            result['ARP']['sender_mac_address'] = self.__parse_arp_sender_mac()
-            result['ARP']['sender_ip_address'] = self.__parse_arp_sender_ip()
-            result['ARP']['target_mac_address'] = self.__parse_arp_target_mac()
-            result['ARP']['target_ip_address'] = self.__parse_arp_target_ip()
+            result['Ethernet']['padding'] = arp['padding']
+            result['ARP']['hardware_type'] = arp['hardware_type']
+            result['ARP']['protocol_type'] = arp['protocol_type']
+            result['ARP']['hardware_size'] = arp['hardware_size']
+            result['ARP']['protocol_size'] = arp['protocol_size']
+            result['ARP']['opcode'] = arp['opcode']
+            result['ARP']['sender_mac_address'] = arp['sender_mac_address']
+            result['ARP']['sender_ip_address'] = arp['sender_ip_address']
+            result['ARP']['target_mac_address'] = arp['target_mac_address']
+            result['ARP']['target_ip_address'] = arp['target_ip_address']
 
         return result
 
@@ -104,7 +133,7 @@ def display(byts):
 
     print('Ethernet:')
     print(f'  Destination MAC Address:  {data["Ethernet"]["destination_mac"]}')
-    print(f'  Source MAC Address:       {data["Ethernet"]["origin_mac"]}')
+    print(f'  Source MAC Address:       {data["Ethernet"]["source_mac"]}')
     print(f'  Protocol:                 {protocol}')
     print(f'  Padding:                  {data["Ethernet"]["padding"]}')
     print()
